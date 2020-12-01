ecd906ec6bdde1ae2f97c48d1a31132ab982e751
Mathieu <mathieu.huot@gmail.com>
2020-11-30 19:08:51 -0500

    Cleaned config file.

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,4 @@
-// Utilities
-const htmlmin = require('html-minifier');
-const uslugify = s => require('uslug')(s);
-const sassProcess = require('./build-process/sass-process');
-const imageProcess = require('./build-process/image-process');
 const { writeFile } = require('fs').promises;
-// Shortcodes
-const card = require('./shortcode/card');
-const contentHeader = require('./shortcode/content-header');
-const searchForm = require('./shortcode/search-form');
-// Filters
-const timeFormat = require('./filters/readable-time');
-const textFormat = require('./filters/text-format');
-const imgFilter = require('./filters/imgFilter');
-// Markdown
-const markdownIt = require('markdown-it');
-const markdownItAnchor = require('markdown-it-anchor');
-const markdownItTocDoneRight = require('markdown-it-toc-done-right');
-const markdownItClass = require('@toycode/markdown-it-class');
 
 module.exports = function(config) {
     // Sass pre-processing
@@ -25,24 +7,8 @@ module.exports = function(config) {
         files: './_site/style/index.css'
     });
     // Passthrough copy
-    const assets = [
-        'images',
-        'fonts',
-        'js',
-        'politiques',
-        './favicon.ico',
-        './manifest.json'
-    ]
+    const assets = []
     assets.forEach(asset => config.addPassthroughCopy(asset));
-    // Shortcodes
-    config.addShortcode('card', card);
-    config.addShortcode('contentHeader', contentHeader);
-    config.addShortcode('searchForm', searchForm);
-    config.addNunjucksAsyncShortcode('img', imageProcess);
-    // Filters
-    config.addFilter('timeFormat', timeFormat);
-    config.addFilter('textFormat', textFormat);
-    config.addNunjucksAsyncFilter('img', imgFilter);
     // Collections
     config.addCollection('posts', collection => collection.getFilteredByGlob('_src/posts/*.md'));
     config.addCollection('postsTags', collection => {
@@ -90,50 +56,8 @@ module.exports = function(config) {
         });*/
         return [...tagSet];
     });
-    // Libraries
-    config.setFrontMatterParsingOptions({
-        excerpt: true,
-        excerpt_separator: '---excerpt---'
-    });
-    config.setLibrary('md', markdownIt ({
-        html: true,
-        breaks: true,
-        linkify: true
-    }).use(markdownItAnchor, {
-        slugify: uslugify,
-        permalink: true,
-        permalinkClass: 'title-link',
-        permalinkSymbol: '#',
-        level: [2, 3]
-    }).use(markdownItTocDoneRight, {
-        slugify: uslugify,
-        listType: 'ul',
-        level: [2, 3],
-        containerClass: 'auto-toc'
-    }).use(markdownItClass, {
-        h2: 'anchors',
-        h3: 'anchors'   
-    }));
-    // Transform
-    config.addTransform('htmlmin', (content, output) => {
-        if(process.env.ELEVENTY_ENV === 'prod' && output.endsWith('.html')) {
-            return htmlmin.minify(content, {
-                useShortDoctype: true,
-				removeComments: true,
-				collapseWhitespace: true
-            });
-        }
-        return content
-    });
     // Aliases
     config.addLayoutAlias('base-layout', 'layouts/base-layout.njk');
-    config.addLayoutAlias('post-layout', 'layouts/post-layout.njk');
-    config.addLayoutAlias('sejour-layout', 'layouts/sejour-layout.njk');
-    config.addLayoutAlias('construction', 'layouts/construction.njk');
-    config.addLayoutAlias('blog-layout', 'layouts/blog-layout.njk');
-    config.addLayoutAlias('formations-layout', 'layouts/formations-layout.njk');
-    config.addLayoutAlias('videos-layout', 'layouts/videos-layout.njk');
-    config.addLayoutAlias('conduite-layout', 'layouts/conduite-layout.njk');
     // Configuration
     return {
         dir: {
